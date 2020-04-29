62eef5a956cc166b45d1a24f507790b863d8a72a
Abiodun Paul Ogunnaike <primastech101@gmail.com>
2020-04-29 22:10:33 +0100

    Arranged names alphabetically based on deejays opinio

--- src/images.js
+++ src/images.js
@@ -2,10 +2,13 @@ import Chelsea from './img/chelsea.png';
 import Manutd from './img/manu.png';
 import Barca from './img/barca.png';
 import Real from './img/real.png';
+import Arsenal from './img/arsenal.png';
+
 
 export const ImgArr = [
     Chelsea,
     Manutd,
     Barca,
-    Real
+    Real,
+    Arsenal
 ];
--- src/kings.js
+++ src/kings.js
@@ -7,14 +7,6 @@ export const kings = [{
         Club: 0
     },
     {
-        Name: 'Hunray',
-        FieldofStudy: 'Public Admin',
-        Certification: 'BSc',
-        Institution: 'OAU',
-        FavoriteColor: 'Orange et Any Primary color',
-        Club: 1
-    },
-    {
         Name: 'Chicago',
         FieldofStudy: 'Business Admin',
         Certification: 'HND',
@@ -23,52 +15,43 @@ export const kings = [{
         Club: 2
     },
     {
-        Name: 'Mikky',
-        FieldofStudy: 'Human Resource Mgt',
-        Certification: 'B.sc',
-        Institution: 'Unilag',
-        FavoriteColor: 'None',
+        Name: 'Chidex',
+        FieldofStudy: 'Com Sc',
+        Certification: 'HND',
+        Institution: 'IMT',
+        FavoriteColor: 'Blue et Red',
+        Club: 0
+    },
+    {
+        Name: 'Deejay',
+        FieldofStudy: 'Banking and Fin',
+        Certification: 'B.Sc',
+        Institution: 'FUNAAB',
+        FavoriteColor: 'White',
         Club: 2
     },
     {
-        Name: 'Nubi',
-        FieldofStudy: 'Mech Engr',
-        Certification: 'B.sc',
-        Institution: 'OOU',
-        FavoriteColor: 'None',
-        Club: 0
-
-    },
-    {
-        Name: 'Nazzy',
-        FieldofStudy: 'Civil Engr',
-        Certification: 'B.Eng',
-        Institution: 'Ansu',
-        FavoriteColor: 'white',
+        Name: 'Great',
+        FieldofStudy: 'Education',
+        Certification: 'B.Sc.(Ed), M.Sc',
+        Institution: 'DELSU, UNILAG',
+        FavoriteColor: 'Green et Red',
         Club: 1
     },
     {
-        Name: 'Pedro',
-        FieldofStudy: 'English language',
-        Certification: 'B.Sc',
-        Institution: 'Unilorin',
-        FavoriteColor: 'Green',
-        Club: 1
+        Name: 'Hunray',
+        FieldofStudy: 'Public Admin',
+        Certification: 'BSc',
+        Institution: 'OAU',
+        FavoriteColor: 'Orange et Any Primary color',
+        Club: 4
     },
     {
-        Name: 'Mackati',
-        FieldofStudy: 'Accounting',
-        Certification: 'B.Sc',
-        Institution: 'NOUN',
-        FavoriteColor: 'Red et black',
-        Club: 3
-    },
-    {
-        Name: 'Wunderkind',
-        FieldofStudy: 'Mech. Engr',
-        Certification: 'B.Sc',
-        Institution: 'OOU',
-        FavoriteColor: 'Grey',
+        Name: 'Kenny',
+        FieldofStudy: 'LG and Dev Studies ',
+        Certification: 'HND , ACPA',
+        Institution: 'IBADAN POLY',
+        FavoriteColor: 'White et Black',
         Club: 1
     },
     {
@@ -80,36 +63,52 @@ export const kings = [{
         Club: 1
     },
     {
-        Name: 'Kenny',
-        FieldofStudy: 'LG and Dev Studies ',
-        Certification: 'HND , ACPA',
-        Institution: 'IBADAN POLY',
-        FavoriteColor: 'White et Black',
-        Club: 1
-    },
-    {
-        Name: 'Great',
-        FieldofStudy: 'Education',
-        Certification: 'B.Sc.(Ed), M.Sc',
-        Institution: 'DELSU, UNILAG',
-        FavoriteColor: 'Green et Red',
-        Club: 1
-    },
-    {
-        Name: 'Chidex',
-        FieldofStudy: 'Com Sc',
-        Certification: 'HND',
-        Institution: 'IMT',
-        FavoriteColor: 'Blue et Red',
-        Club: 0
-    },
-    {
-        Name: 'Ayodeji',
-        FieldofStudy: 'Banking and Fin',
+        Name: 'Mackati',
+        FieldofStudy: 'Accounting',
         Certification: 'B.Sc',
-        Institution: 'FUNAAB',
-        FavoriteColor: 'White',
+        Institution: 'NOUN',
+        FavoriteColor: 'Red et black',
+        Club: 3
+    },
+    {
+        Name: 'Mikky',
+        FieldofStudy: 'Human Resource Mgt',
+        Certification: 'B.sc',
+        Institution: 'Unilag',
+        FavoriteColor: 'None',
         Club: 2
-    }
+    },
+    {
+        Name: 'Nazzy',
+        FieldofStudy: 'Civil Engr',
+        Certification: 'B.Eng',
+        Institution: 'Ansu',
+        FavoriteColor: 'white',
+        Club: 1
+    },
+    {
+        Name: 'Nubi',
+        FieldofStudy: 'Mech Engr',
+        Certification: 'B.sc',
+        Institution: 'OOU',
+        FavoriteColor: 'None',
+        Club: 0
 
+    },
+    {
+        Name: 'Pedro',
+        FieldofStudy: 'English language',
+        Certification: 'B.Sc',
+        Institution: 'Unilorin',
+        FavoriteColor: 'Green',
+        Club: 1
+    },
+    {
+        Name: 'Wunderkind',
+        FieldofStudy: 'Mech. Engr',
+        Certification: 'B.Sc',
+        Institution: 'OOU',
+        FavoriteColor: 'Grey',
+        Club: 1
+    }
 ]

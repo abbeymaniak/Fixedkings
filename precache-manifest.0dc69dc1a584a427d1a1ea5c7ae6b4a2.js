0f0822f499bacaef9d773d655e8471afb497cc30
Abiodun Paul Ogunnaike <primastech101@gmail.com>
2020-04-29 23:04:24 +0100

    Updates

---
+++ precache-manifest.0dc69dc1a584a427d1a1ea5c7ae6b4a2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d236e0320f53bc758b14fd60d391d1c",
+    "url": "/Fixedkings/index.html"
+  },
+  {
+    "revision": "ec4e07b2f11de4ea102e",
+    "url": "/Fixedkings/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "2acfa553cc9659387ce8",
+    "url": "/Fixedkings/static/css/main.08a1df0f.chunk.css"
+  },
+  {
+    "revision": "ec4e07b2f11de4ea102e",
+    "url": "/Fixedkings/static/js/2.19a64bbe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Fixedkings/static/js/2.19a64bbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2acfa553cc9659387ce8",
+    "url": "/Fixedkings/static/js/main.3995b4a1.chunk.js"
+  },
+  {
+    "revision": "298be43a7d0a8fef42e5",
+    "url": "/Fixedkings/static/js/runtime-main.a342bdab.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Fixedkings/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "20c5a666edc396eac260cbc481586ef1",
+    "url": "/Fixedkings/static/media/arsenal.20c5a666.png"
+  },
+  {
+    "revision": "bb0959e6b18f86ca86b7ceb68a16ff45",
+    "url": "/Fixedkings/static/media/barca.bb0959e6.png"
+  },
+  {
+    "revision": "d854acffbd8f25498a02f989608016f9",
+    "url": "/Fixedkings/static/media/chelsea.d854acff.png"
+  },
+  {
+    "revision": "aaa70615902fea5ce3045ed5151f2764",
+    "url": "/Fixedkings/static/media/manu.aaa70615.png"
+  },
+  {
+    "revision": "f04c74af108d7884ca1d0b7c9c2e3808",
+    "url": "/Fixedkings/static/media/real.f04c74af.png"
+  }
+]);
